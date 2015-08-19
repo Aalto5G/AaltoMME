6c92f0582f7a4947a0dad57023aa1d5b50084e3f
Vicent Ferrer <vicent.ferrerguasch@aalto.fi>
2015-08-19 12:37:22 +0300

    S1Assoc_active, started processMsg

--- mme/S1/S1Assoc_Active.c
+++ mme/S1/S1Assoc_Active.c
@@ -19,8 +19,46 @@
 #include "S1Assoc_priv.h"
 #include "S1Assoc_FSMConfig.h"
 
-static void processMsg(gpointer self, S1AP_Message_t *msg, int r_sid){
-	log_msg(LOG_ERR, 0, "Not Implemented");
+static void processMsg(gpointer _assoc, S1AP_Message_t *s1msg, int r_sid){
+	S1Assoc_t *assoc = (S1Assoc_t *)_assoc;
+
+	if(r_sid == assoc->nonue_rsid){
+		/* ************************************************** */
+		/*             Non UE Associated Signaling            */
+		/* ************************************************** */
+		if(s1msg->pdu->procedureCode == id_Reset){
+		}else if(s1msg->pdu->procedureCode == id_initialUEMessage &&
+		         s1msg->choice == initiating_message){
+			log_msg(LOG_WARNING, 0, "Received Initial UE Message");
+		}else if(s1msg->pdu->procedureCode == id_ErrorIndication &&
+		         s1msg->choice == initiating_message){
+			log_msg(LOG_WARNING, 0, "Received Error Indication");
+		}else if(s1msg->pdu->procedureCode == id_eNBConfigurationTransfer &&
+		         s1msg->choice == initiating_message){
+			log_msg(LOG_WARNING, 0, "Received eNB Configuration Transfer");
+		}else if(s1msg->pdu->procedureCode == id_ENBConfigurationUpdate &&
+		         s1msg->choice == initiating_message){
+			log_msg(LOG_WARNING, 0, "Received eNB Configuration Update");
+		}else if(s1msg->pdu->procedureCode == id_PathSwitchRequest &&
+		         s1msg->choice == initiating_message){
+			log_msg(LOG_WARNING, 0, "Received Path Switch Request");
+		}else if(s1msg->pdu->procedureCode == id_WriteReplaceWarning &&
+		         s1msg->choice == successful_outcome){
+			log_msg(LOG_WARNING, 0, "Received Write ReplaceWarning Response");
+		}else if(s1msg->pdu->procedureCode == id_Kill &&
+		         s1msg->choice == successful_outcome){
+			log_msg(LOG_WARNING, 0, "Received Kill Response");
+		}else{
+			log_msg(LOG_WARNING, 0, "Received %s on non UE associated signaling, ignoring",
+			        elementaryProcedureName[s1msg->pdu->procedureCode]);
+			return;
+		}
+	}else{
+		/* ************************************************** */
+		/*               UE associated signaling              */
+		/* ************************************************** */
+
+	}
 }
 
 
