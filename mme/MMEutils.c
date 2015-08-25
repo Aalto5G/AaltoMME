15221c75b4b2aed6f141a3949d50174496931641
Vicent Ferrer <vicent.ferrerguasch@aalto.fi>
2015-08-25 13:44:06 +0300

    Add MMEutils I forgot before

---
+++ mme/MMEutils.c
@@ -0,0 +1,96 @@
+/* This application was initially developed as a Final Project by
+ *     Vicent Ferrer Guasch (vicent.ferrerguasch@aalto.fi)
+ * under the supervision of,
+ *     Jukka Manner (jukka.manner@aalto.fi)
+ *     Jose Costa-Requena (jose.costa@aalto.fi)
+ * in AALTO University and partially funded by EIT ICT labs.
+ */
+
+/**
+ * @file   MMEutils.c
+ * @Author Vicent Ferrer
+ * @date   August, 2015
+ * @brief  
+ *
+ * 
+ */
+
+#include <string.h>
+#include "MMEutils.h"
+#include "logmgr.h"
+#include "NAS.h"
+
+void plmn_FillFromTBCD(mme_PLMN *plmn, const guint8 *tbcd){
+    memcpy(plmn->tbcd, tbcd, 3);
+}
+
+void plmn_FillFromMCCMNCs(mme_PLMN *plmn, const guint16 mcc, const guint16 mnc){
+    guint8 *tmp = plmn->tbcd;
+    tmp[0] = ((mcc%100)/10<<4) | mcc/100;
+    tmp[1] = (mcc%10);
+    if(mnc/100 == 0){
+        tmp[1]|=0xf0;
+        tmp[2] = (mnc/10) | (mnc%10)<<4;
+    }else{
+        tmp[1]|= (mnc%10)<<4;
+        tmp[2] = ((mnc%100)/10<<4) | mnc/100;
+    }
+}
+
+gboolean plmn_Equal(const mme_PLMN *a, const mme_PLMN *b){
+	if (! memcmp(a->tbcd, b->tbcd, 3 * sizeof(guint8)))
+		return TRUE;
+	else
+		return FALSE;
+}
+
+guint plmn_Hash(const mme_PLMN *key){
+	guint32 nk = (key->tbcd[2] *10000) & (key->tbcd[1] * 100) & key->tbcd[0];
+	return g_int_hash(&nk);
+}
+
+
+mme_GlobaleNBid *globaleNBID_Fill(mme_GlobaleNBid *r, const Global_ENB_ID_t *gid){
+	plmn_FillFromTBCD( &(r->plmn), gid->pLMNidentity->tbc.s);
+	if (gid->eNBid->choice == 0){
+		r->macroId = TRUE;
+		r->eNBid = gid->eNBid->id.macroENB_ID<<4;
+	}else{
+		r->macroId = FALSE;
+		r->eNBid = gid->eNBid->id.homeENB_ID<<12;
+	}
+}
+
+mme_GlobaleNBid *globaleNBID_copy(mme_GlobaleNBid *in, mme_GlobaleNBid *out){
+	plmn_FillFromTBCD( &(out->plmn), in->plmn.tbcd);
+	out->macroId = in->macroId;
+	out->eNBid = in->eNBid;
+	return out;
+}
+
+const guint globaleNB_getMCC(const mme_GlobaleNBid *gid){
+	return ((gid->plmn.tbcd[0]&0x0f))*100 +
+		(gid->plmn.tbcd[0]>>4)*10 +
+		((gid->plmn.tbcd[1]&0x0f));
+}
+
+const guint globaleNB_getMNC(const mme_GlobaleNBid *gid){
+	return ((gid->plmn.tbcd[1]>>4)==0xf)?
+		(gid->plmn.tbcd[2]&0x0f)*10 + (gid->plmn.tbcd[2]>>4):
+		(gid->plmn.tbcd[1]>>4)*100 + (gid->plmn.tbcd[2]&0x0f)*10 + (gid->plmn.tbcd[2]>>4);
+}
+
+const guint globaleNB_getCI(const mme_GlobaleNBid *gid){
+	return gid->eNBid;
+}
+
+gboolean globaleNBID_Equal(const mme_GlobaleNBid *a, const mme_GlobaleNBid *b){
+	return plmn_Equal(&(a->plmn), &(b->plmn)) &&
+		(a->macroId == b->macroId) &&
+		(a->eNBid == b->eNBid);
+}
+
+guint globaleNBID_Hash(const mme_GlobaleNBid *k){
+	guint id = k->eNBid;
+	return plmn_Hash(&(k->plmn)) ^ g_int_hash(&id);
+}
---
+++ mme/MMEutils.h
@@ -0,0 +1,71 @@
+/* This application was initially developed as a Final Project by
+ *     Vicent Ferrer Guasch (vicent.ferrerguasch@aalto.fi)
+ * under the supervision of,
+ *     Jukka Manner (jukka.manner@aalto.fi)
+ *     Jose Costa-Requena (jose.costa@aalto.fi)
+ * in AALTO University and partially funded by EIT ICT labs.
+ */
+
+/**
+ * @file   MMEutils.h
+ * @Author Vicent Ferrer
+ * @date   August, 2015
+ * @brief  
+ *
+ * 
+ */
+
+#ifndef MMEUTILS_H
+#define MMEUTILS_H
+
+#include <glib.h>
+#include "S1AP.h"
+
+typedef struct{
+	guint8 tbcd[3];
+}mme_PLMN;
+
+typedef struct{
+	mme_PLMN    plmn;
+	gboolean    macroId;
+	guint32     eNBid:28;
+}mme_GlobaleNBid;
+
+/**
+ * @brief fill the PLMN struct
+ * @param [out] plmn struct to be filled
+ * @param [in] tbcd input data
+ *
+ * The format of the input is TBCD
+ */
+void plmn_FillFromTBCD(mme_PLMN *plmn, const guint8 *tbcd);
+
+/**
+ * @brief fill the PLMN struct
+ * @param [out] plmn struct to be filled
+ * @param [in] mcc input data
+ * @param [in] mnc input data
+ *
+ * The format of the input is MCC, MNC
+ */
+void plmn_FillFromMCCMNCs(mme_PLMN *plmn, const guint16 mcc, const guint16 mnc);
+
+
+gboolean plmn_Equal(const mme_PLMN *a, const mme_PLMN *b);
+
+guint plmn_Hash(const mme_PLMN *key);
+
+
+mme_GlobaleNBid *globaleNBID_Fill(mme_GlobaleNBid *r, const Global_ENB_ID_t *gid);
+mme_GlobaleNBid *globaleNBID_copy(mme_GlobaleNBid *in, mme_GlobaleNBid *out);
+
+const guint globaleNB_getMCC(const mme_GlobaleNBid *gid);
+const guint globaleNB_getMNC(const mme_GlobaleNBid *gid);
+const guint globaleNB_getCI(const mme_GlobaleNBid *gid);
+
+gboolean globaleNBID_Equal(const mme_GlobaleNBid *a, const mme_GlobaleNBid *b);
+
+guint globaleNBID_Hash(const mme_GlobaleNBid *k);
+
+
+#endif /* MMEUTILS_H*/
