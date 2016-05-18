30224f08cfdeb94fe14e349b886f7ce63fd40146
Vicent Ferrer Guasch <vicent.ferrerguasch@aalto.fi>
2016-05-18 15:15:57 +0000

    Added Unit file for Systemd. Adapted logging.

--- Common/logmgr.c
+++ Common/logmgr.c
@@ -43,60 +43,66 @@ static struct log_entity logger;
 static char* logLevelStr[] = {"EMER", "ALER", "CRIT", "ERRO", "WARN", "NOTI", "INFO", "DEBG"};
 
 void init_logger(const char * app, int priority){
-  openlog(app, (LOG_PID | LOG_PERROR), LOG_USER);
+  /*openlog(app, (LOG_PID | LOG_PERROR), LOG_USER);*/
   logger.priority = priority;
   setlogmask (LOG_UPTO (priority));
   log_msg(LOG_INFO, 0, "Log initialized with level %s", logLevelStr[priority]);
 }
 
 void close_logger(){
-    log_msg(LOG_INFO, 0, "Closing Log");
-    closelog();
+  log_msg(LOG_INFO, 0, "Closing Log");
+  /*closelog();*/
 }
 
 
 void change_logger_lvl(int priority){
-    if(priority>=0 && priority<8){
-        setlogmask (LOG_UPTO (priority));
-        logger.priority=priority;
-        log_msg(LOG_INFO, 0, "Changed log level: %s", logLevelStr[priority]);
-    }else{
-        log_msg(LOG_ERR, 0, "Couldn't Change Log level, value incorrect %d", priority);
-    }
+  if(priority>=0 && priority<8){
+    setlogmask (LOG_UPTO (priority));
+    logger.priority=priority;
+    log_msg(LOG_INFO, 0, "Changed log level: %s", logLevelStr[priority]);
+  }else{
+    log_msg(LOG_ERR, 0, "Couldn't Change Log level, value incorrect %d", priority);
+  }
 }
 
 
 void log_msg_s(int pri, char *fn, const char *func, int ln, int en, const char *msg) {
-
-    char timeStr[128];
-    struct timeval tv;
-    struct tm *pTm;
-    gettimeofday( &tv, NULL );
-    pTm = localtime ( (const time_t*)&tv.tv_sec );
-    strftime ((char*) timeStr, 128, "%d/%m/%Y %T", pTm );
-    if(logger.priority >= LOG_DEBUG){
-        sprintf((char*)(timeStr + strlen((char*)timeStr)),
-                ".%06u",
-                (unsigned int)tv.tv_usec);
+  char timeStr[128];
+  struct timeval tv;
+  struct tm *pTm;
+  gettimeofday( &tv, NULL );
+  pTm = localtime ( (const time_t*)&tv.tv_sec );
+  strftime ((char*) timeStr, 128, "%d/%m/%Y %T", pTm );
+  if(logger.priority >= LOG_DEBUG){
+    sprintf((char*)(timeStr + strlen((char*)timeStr)),
+            ".%06u",
+            (unsigned int)tv.tv_usec);
+  }
+ /* if(logger.priority >= LOG_DEBUG){ */
+  if(0){
+	if (en)
+      syslog(pri, "[%s] %s - %d (%s) %s <%s(), %s:%d>",
+             timeStr, logLevelStr[pri], en, strerror(en), msg,
+             func, basename(fn), ln);
+    else
+	  syslog(pri, "[%s] %s - %s <%s(), %s:%d>",
+	         timeStr, logLevelStr[pri], msg,
+             func, basename(fn), ln);
+  }else if(0){
+	if (en)
+      syslog(pri, "[%s] %s - %d (%s) %s",
+             timeStr, logLevelStr[pri], en, strerror(en), msg);
+    else
+      syslog(pri, "[%s] %s - %s", timeStr, logLevelStr[pri], msg);
+  }else{
+    if(pri <= logger.priority){
+      if (en)
+        printf("%s - %d (%s) %s\n",
+               logLevelStr[pri], en, strerror(en), msg);
+      else
+        printf("%s - %s\n", logLevelStr[pri], msg);
     }
-
-    /* if(logger.priority >= LOG_DEBUG){ */
-    if(1){
-        if (en)
-            syslog(pri, "[%s] %s - %d (%s) %s <%s(), %s:%d>",
-                   timeStr, logLevelStr[pri], en, strerror(en), msg,
-                   func, basename(fn), ln);
-        else
-            syslog(pri, "[%s] %s - %s <%s(), %s:%d>",
-                   timeStr, logLevelStr[pri], msg,
-                   func, basename(fn), ln);
-    }else{
-        if (en)
-            syslog(pri, "[%s] %s - %d (%s) %s",
-                   timeStr, logLevelStr[pri], en, strerror(en), msg);
-        else
-            syslog(pri, "[%s] %s - %s", timeStr, logLevelStr[pri], msg);
-    }
+  }
 }
 
 
@@ -149,13 +155,13 @@ void log_errpack_(int pri, char *fn, const char *func, int ln, int en, struct so
 
 
 void printfbuffer(uint8_t *buf, uint32_t size){
-    uint32_t i;
-    printf("The buffer looks like: n %u\n", size);
-    for( i = 0; i < size; i=i+2) {
-        printf("%.2x%.2x ", buf[i], buf[i+1]);
-        if(i%16==14){
-            printf("\n");
-        }
+  uint32_t i;
+  printf("The buffer looks like: n %u\n", size);
+  for( i = 0; i < size; i=i+2) {
+    printf("%.2x%.2x ", buf[i], buf[i+1]);
+    if(i%16==14){
+      printf("\n");
     }
-    printf("\n");
+  }
+  printf("\n");
 }
