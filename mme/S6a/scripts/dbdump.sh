359d50fa60b281e21dd98d20ec845f53301a9c57
Vicent Ferrer Guasch <vicent.ferrerguasch@aalto.fi>
2016-10-18 13:43:11 +0000

    Avoid using binary on database dump files

--- mme/S6a/scripts/dbdump.sh
+++ mme/S6a/scripts/dbdump.sh
@@ -8,8 +8,4 @@ echo "# DB access rights" >>$FILE
 echo "grant delete,insert,select,update on hss_lte_db.* to hss@localhost identified by 'hss';" >>$FILE
 
 FILE=userdata.sql
-mysqldump hss_lte_db --single-transaction -u hss -p -t -B  >$FILE
-
-
-
-
+mysqldump hss_lte_db --hex-blob --single-transaction -u hss -p -t -B  >$FILE
